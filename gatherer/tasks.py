b947e4abe3293fbccf137ac9b24f1769f3459bdf
Helgefy <helge_fylkesnes@hotmail.com>
2017-09-14 21:09:34 +0200

    inital commit

--- gatherer/tasks.py
+++ gatherer/tasks.py
@@ -16,7 +16,7 @@ logger=get_task_logger(__name__)
 
 
 consumer_key =  os.environ.get('CONSUMER_KEY')
-consumer_secret = os.environ.get('CONSUMER_SECRET ')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
